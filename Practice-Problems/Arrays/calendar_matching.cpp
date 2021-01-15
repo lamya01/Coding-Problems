79eb673532cdecb2a58ea5c4fbacac770c618bae
lamya01 <lamyabhasin01@gmail.com>
2021-01-15 11:04:43 +0530

    calendar matchingsolution

---
+++ Practice-Problems/Arrays/calendar_matching.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <strings>
+using namespace std;
+
+struct StringMeeting
+{
+    string s;
+    string e;
+    StringMeeting(string a, string b)
+    {
+        s = a, e = b;
+    }
+    StringMeeting()
+    bool operator < (string s) 
+    { 
+        string l = stoi(this->)
+        string r = stoi(s);
+         
+}
